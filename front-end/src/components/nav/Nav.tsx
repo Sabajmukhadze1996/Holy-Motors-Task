f1a2b7b40fe5b9b650cc837f09a599c93f8efcf2
Saba Jmukhadze <sabajmukhadze23@gmail.com>
2024-06-25 10:59:30 +0400

    (front-end) Created basic folder structure, added Nav component and images

---
+++ front-end/src/components/nav/Nav.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./nav.css"
+
+
+
+const Nav = () => {
+   return (
+    <nav>
+      <a href=""><span>OUR</span>PROJECTS</a>
+      <a href=""><span>OUR</span>WORKS</a>
+      <a href=""><span>OUR</span>CLIENTS</a>
+      <a href=""><span>OUR</span>STORY</a>
+    </nav>
+   )
+}
+
+
+export default Nav;
