1fd05c6aa71f819f2c48b93681c01c3a73f11b0c
Saba Jmukhadze <sabajmukhadze23@gmail.com>
2024-06-27 23:12:22 +0400

    new folder structure created for code readability

--- back-end/models/OurProjectsModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const OurProjectsSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  img: { type: String, required: true },
-});
-
-module.exports = mongoose.model("OurProjects", OurProjectsSchema);
---
+++ back-end/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schemas');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB', err);
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
