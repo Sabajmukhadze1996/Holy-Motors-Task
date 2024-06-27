6bd3496a43a6c7b39fca6b40d2f2ed019c4d3736
Saba Jmukhadze <sabajmukhadze23@gmail.com>
2024-06-28 00:31:12 +0400

    (Front-End) our works data fetched from server successfully

--- front-end/src/components/our-works/OurWorks.tsx
+++ front-end/src/components/our-works/OurWorks.tsx
@@ -1,48 +1,38 @@
 import React, { useState } from "react";
+import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
 import "./our-works.css";
 import left_arrow from "../../images/our-projects/left-arrow.png";
 import right_arrow from "../../images/our-projects/right-arrow.png";
-import slide_one from "../../images/our-work/slide-01.png";
-import slide_two from "../../images/our-work/slide-02.png";
-import slide_three from "../../images/our-work/slide-03.png";
-import slide_four from "../../images/our-work/slide-04.png";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const GET_WORKS = gql`
+  query GetWorks {
+    works {
+      id
+      title
+      description
+      img
+    }
+  }
+`;
 
 const OurWorks = () => {
+  const { loading, error, data } = useQuery(GET_WORKS, { client });
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const data = [
-    {
-      id: 1,
-      title: "MEGOBAR - GEORGIAN FOOD SHOP",
-      description: `Friendliest food in the neighborhood Megobar is a new chain of food shops with a mission to make delicious Georgian food accessible to everyone around the world. Megobar (mdmn) means "a friend" in Georgian, and originally only referred to people you share a meal with, proving that food and friendship are colossally important to our culture.`,
-      img: slide_one
-    },
-    {
-      id: 2,
-      title: "BLAUENSTEIN FARM",
-      description: "Blauenstein is a Swiss-Georgian company creating the finest meat products in the pristine highlands of Racha, Georgia. With a culture of respect for nature and local traditions, Swiss-trained professionals and absolute control over the entire chain of production, Blauenstein shops are always full of meat connoisseurs.",
-      img: slide_two
-    },
-    {
-      id: 3,
-      title: "CCLA - LAW CONSULTING AGENCY",
-      description: "Out of nothing comes the brand new visual identity for CCLA, a Tbilisi-based law consulting agency which uses creativity to open the right doors for its clients.",
-      img: slide_three
-    },
-    {
-      id: 4,
-      title: "WOMEN FROM THE BAZAAR",
-      description: "This is not Harper’s Bazaar. The exclusive issue of Women From The Bazaar unites six trader women from Tbilisi's open-air markets. In addition to working 70 hours a week, trader women are often synonymous with waist bags, which are used as self-made cash registers. In collaboration with Cosmo, a handmade accessories brand, we decided to launch an extraordinary fashion magazine about universal, everyday fashion.",
-      img: slide_four
-    },
-  ];
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
   const handlePrevSlide = () => {
-    setCurrentSlide((prev) => (prev === 0 ? data.length - 1 : prev - 1));
+    setCurrentSlide((prev) => (prev === 0 ? data.works.length - 1 : prev - 1));
   };
 
   const handleNextSlide = () => {
-    setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    setCurrentSlide((prev) => (prev === data.works.length - 1 ? 0 : prev + 1));
   };
 
   const handleIndicatorClick = (index: any) => {
@@ -53,7 +43,7 @@ const OurWorks = () => {
     <section className="our-work-container" id="our-works">
       <div className="inner-container">
         <div className="carousel-indicators">
-          {data.map((slide, index) => (
+          {data.works.map((slide: any, index: any) => (
             <button
               key={index}
               type="button"
@@ -89,10 +79,10 @@ const OurWorks = () => {
           </div>
         </div>
 
-        <img src={data[currentSlide].img} alt="img" className="img-fluid" />
+        <img src={data.works[currentSlide].img} alt="img" className="img-fluid" />
         <div className="bottom-container">
-          <p>{data[currentSlide].description}</p>
-          <p className="card-title">{data[currentSlide].title}</p>
+          <p>{data.works[currentSlide].description}</p>
+          <p className="card-title">{data.works[currentSlide].title}</p>
         </div>
       </div>
     </section>
