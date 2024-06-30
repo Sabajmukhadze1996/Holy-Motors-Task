3aa042749f1f6c0e847830b7a63a4e7f7470fdac
Saba Jmukhadze <sabajmukhadze23@gmail.com>
2024-06-30 11:00:10 +0400

    (Baack-End) cors configuration changed to public

--- back-end/src/middlewares/cors.js
+++ back-end/src/middlewares/cors.js
@@ -2,14 +2,7 @@ const cors = require("cors");
 
 module.exports.addCors = () => {
   return cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ["http://localhost:3000"];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true, 
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   });
